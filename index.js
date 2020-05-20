b559604a16f699b12068336c020199dbfffd3a7f
Connor Sullivan <crsullivan@protonmail.com>
2020-05-20 15:03:20 -0700

    adds web socket

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 const server = require('./api/server.js');
+var app = require('express')();
+var http = require('http').createServer(server);
+var io = require('socket.io')(http);
+
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/index.html');
+// });
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
 
 const PORT = process.env.PORT || 7777;
-server.listen(PORT, () => {
+http.listen(PORT, () => {
   console.log(`\n=== Server listening on port ${PORT} ===\n`);
 });
+
+
+
+
+
+
+// const server = require('./api/server.js');
+
+// const PORT = process.env.PORT || 7777;
+// server.listen(PORT, () => {
+//   console.log(`\n=== Server listening on port ${PORT} ===\n`);
+// });
